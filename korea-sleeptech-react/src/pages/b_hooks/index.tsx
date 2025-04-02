6a17266bb6e475a4fa51785cd560a828860d4fa2
sun4584 <shkrtyks@naver.com>
2025-04-02 09:50:05 +0900

    korea

--- korea-sleeptech-react/src/pages/b_hooks/index.tsx
+++ korea-sleeptech-react/src/pages/b_hooks/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+
 import UseState01 from "./a_useState/UseState01";
 import UseState02 from "./a_useState/UseState02";
 import UseState03 from "./a_useState/UseState03";
 import UseState04 from "./a_useState/UseState04";
 import UseState05 from "./a_useState/UseState05";
 import UseState06 from "./a_useState/UseState06";
-import Practice from "./a_useState/Practice";
+import UseStatePractice from "./a_useState/Practice";
+
+import UseRef01 from "./b_useRef/UseRef01";
+import UseRef02 from "./b_useRef/UseRef02";
+import UseRefPractice from "./b_useRef/Practice";
+import UseEffect01 from "./c_useEffect/UseEffect01";
 
 const h2Style = {
   backgroundColor: "black",
@@ -24,6 +30,14 @@ function Index() {
         === 리액트 Hooks ===
       </h1>
 
+      <h2 style={h2Style}>리액트 Hooks - useEffect</h2>
+      <UseEffect01 />
+
+      <h2 style={h2Style}>리액트 Hooks - useRef</h2>
+      <UseRef01 />
+      <UseRef02 />
+      <UseRefPractice />
+
       <h2 style={h2Style}>리액트 Hooks - useState</h2>
       <UseState01 /> <hr />
       <UseState02 /> <hr />
@@ -31,7 +45,8 @@ function Index() {
       <UseState04 /> <hr />
       <UseState05 /> <hr />
       <UseState06 /> <hr />
-      <Practice />
+      <UseStatePractice />
+
     </div>
   );
 }
