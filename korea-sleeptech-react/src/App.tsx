caae641cebf7ceaea6eb7431f1293faf3eb8536f
sun4584 <shkrtyks@naver.com>
2025-04-07 17:06:11 +0900

    korea_sleepTech_front

--- korea-sleeptech-react/src/App.tsx
+++ korea-sleeptech-react/src/App.tsx
@@ -2,11 +2,18 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 //# 컴포넌트 import #//
+import NaviBar from './components/NaviBar';
 
 //? cf) index 파일명 - 하나의 폴더 내에서 한 번만 사용 가능!
 //  >> index.tsx 파일은 해당 파일이 포함된 폴더의 메인 파일로 인식!
 //  >> import 시 폴더명 만으로 가져오기 가능!
-import Basic from '@/pages/a_basic'; // '@/pages/a_basic/index.tsx'와 동일
+import Basic from '@/pages/a_basic/index'; // '@/pages/a_basic/index.tsx'와 동일
+import Hooks from '@/pages/b_hooks';
+import Router from '@/pages/c_router';
+import RouterPractice from '@/pages/d_router_practice';
+import Review from '@/pages/e_review';
+import Http from '@/pages/f_http';
+import GlobalState from '@/pages/g_global_state';
 
 //& 기본 Vite React 앱의 경로
 // : http://localhost:5173
@@ -14,6 +21,8 @@ function App() {
   return (
     <>
       <h1>Korea SleepTech React</h1>
+      <NaviBar />
+
       {/* Routes 태그: Route를 감싸는 컴포넌트 */}
       <Routes>
         {/* Route 태그: 단일 태그 사용 권장! */}
@@ -22,6 +31,19 @@ function App() {
           http://localhost:5173/basic
         */}
         <Route path='/basic' element={<Basic />} />
+        <Route path='/hooks' element={<Hooks />} />
+
+        {/* 
+          중첩 라우팅
+          : 해당 컴포넌트의 경로 내부에서 라우트 경로에 따라 페이지 전환이 일어남을 명시
+
+          path='/해당컴포넌트기본경로/*'
+        */}
+        <Route path='/router/*' element={<Router />} />
+        <Route path='/router-practice/*' element={<RouterPractice />} />
+        <Route path='/review/*' element={<Review />} />
+        <Route path='/http' element={<Http />} />
+        <Route path='/global-state' element={<GlobalState />} />
       </Routes>
     </>
   )
